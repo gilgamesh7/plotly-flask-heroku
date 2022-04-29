5717b80f62f5dadd4bcf13c0af480c7d33081617
gilgamesh7 <felidaesrule@yahoo.com>
2022-04-29 15:49:47 +1200

    fix templates directory

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template
 
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    bar=create_plot()
+    return render_template('index.html', plot=bar)
+
+def create_plot():
+    N = 40
+    x = np.linspace(0,1,N)
+    y = np.random.randn(N)
+
+    df = pd.DataFrame({'x': x, 'y': y})
+
+    data = [
+        go.Bar(
+            x=df['x']
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.JSONEncoder)
+
+    return graphJSON
 
 if __name__ == '__main__':
     app.run(debug=True)
